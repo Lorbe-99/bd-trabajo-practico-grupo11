8a060cb777ff277c135a95518925076f4225a48f
Lucas Orbe <111987017+Lorbe-99@users.noreply.github.com>
2025-05-25 21:45:06 -0300

    Delete 02_InfoMed_TP5_OrbeSanchez.sql

--- 02_InfoMed_TP5_OrbeSanchez.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW EdadesPacientes AS
-SELECT
-id_paciente,
-    nombre,
-fecha_nacimiento, 
-TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE()) AS edad
-FROM 
-Pacientes;
-SELECT * FROM EdadesPacientes
