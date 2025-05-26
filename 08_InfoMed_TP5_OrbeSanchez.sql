ba7cadb57a16a7fa1f403e33bb305c280b3be318
Lucas Orbe <111987017+Lorbe-99@users.noreply.github.com>
2025-05-25 21:46:30 -0300

    Delete 08_InfoMed_TP5_OrbeSanchez.sql

--- 08_InfoMed_TP5_OrbeSanchez.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    SUM(case when p.id_sexo = 1 THEN 1 ELSE 0 END) AS Hombres,
-    SUM(case when p.id_sexo = 2 THEN 1 ELSE 0 END) AS Mujeres,
-    c.nombre_cuidad AS Cuidad
-FROM
-    Pacientes p
-    LEFT JOIN Cuidades c ON p.id_cuidad = c.id_cuidad
-    LEFT JOIN SexoBiologico s ON p.id_sexo = c.id_sexo
-GROUP BY c.nombre_cuidad
