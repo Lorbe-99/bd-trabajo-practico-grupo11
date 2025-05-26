f3d337eae59490ec71d55f5571434de2af36a40e
Camila Sanchez <90740493+gintonic06@users.noreply.github.com>
2025-05-25 21:58:37 -0300

    Query 14

---
+++ 14_InfoMed_TP5_OrbeSanchez.sql
@@ -0,0 +1,11 @@
+SELECT
+	mt.nombre AS Medicamento,
+    COUNT(r.id_medicamento) AS VecesRecetada
+FROM
+	Medicamentos mt
+    LEFT JOIN Recetas r ON r.id_medicamento = mt.id_medicamento
+GROUP BY	
+	mt.id_medicamento
+ORDER BY
+    VecesRecetada DESC
+LIMIT 1;
