8b35e5e19966189bce4483897f4197f48a59fb4f
Camila Sanchez <90740493+gintonic06@users.noreply.github.com>
2025-05-25 21:24:52 -0300

    Delete 14_InfoMed_TP5_OrbeSanchez.sql

--- 14_InfoMed_TP5_OrbeSanchez.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-	mt.nombre AS Medicamento,
-    COUNT(r.id_medicamento) AS VecesRecetada
-FROM
-	Medicamentos mt
-    LEFT JOIN Recetas r ON r.id_medicamento = mt.id_medicamento
-GROUP BY	
-	mt.id_medicamento
